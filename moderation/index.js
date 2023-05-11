c27de93cd46d36076f53ebfadb303a42087f7fa8
Narasimhan N <Narasimhan.N@iiitb.ac.in>
2023-05-12 00:01:26 +0530

    MODERATION service parsing function updated

--- moderation/index.js
+++ moderation/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
@@ -5,12 +7,25 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+function containsKeywords(sentence, keywords) {
+  for (const keyword of keywords) {
+    if (sentence.includes(keyword)) {
+      return true;
+    }
+  }
+  return false;
+}
+const keywords = ["fuck", "asshole", "mental"];
+
 app.post("/events", async (req, res) => {
   const { type, data } = req.body;
 
   if (type === "ReviewCreated") {
     console.log(type);
-    const status = data.content.includes("orange") ? "rejected" : "approved";
+    //const status = data.content.includes("orange") ? "rejected" : "approved";
+    const status = containsKeywords(data.content, keywords)
+      ? "rejected"
+      : "approved";
     await axios.post("http://eventbus-srv:4005/events", {
       type: "ReviewModerated",
       data: {
