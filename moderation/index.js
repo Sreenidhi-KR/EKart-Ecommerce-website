800b95b4d9d6e7c65bf13f8f43bc1261202eb11b
Sreenidhi <Rsri1998@gmail.com>
2023-05-16 02:05:52 +0530

    minor fix

--- moderation/index.js
+++ moderation/index.js
@@ -2,8 +2,9 @@ const { app, handleEvent } = require("./app");
 const axios = require("axios");
 
 app.listen(4003, async () => {
+  console.log("Moderation Listening on 4003");
   setTimeout(async function () {
-    console.log("Moderation Listening on 4003");
+    console.log("Moderation Checking for any missed events");
     try {
       const res = await axios.get(
         "http://eventbus-srv:4005/failedEvents/moderation"
--- products/index.js
+++ products/index.js
@@ -1,8 +1,9 @@
 const { app, handleEvent } = require("./app");
 const axios = require("axios");
 app.listen(4000, async () => {
+  console.log("Products Listening on - 4000");
   setTimeout(async function () {
-    console.log("Products Listening on - 4000");
+    console.log("Products Checking for any missed events");
     try {
       const res = await axios.get(
         "http://eventbus-srv:4005/failedEvents/products"
